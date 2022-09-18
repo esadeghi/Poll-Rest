c01a24a58929c4ffc20267aa058c58b19b279b3c
Ehsan Sadeghi <esadeghi@gmail.com>
2022-09-18 15:19:14 +0430

    Add Initial Empty Project

---
+++ src/test/java/com/ehsan/pollrest/PollRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehsan.pollrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
